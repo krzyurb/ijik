cd18e5b206b8676993f891595cfe7885b111ce06
krzyurb <11517908+krzyurb@users.noreply.github.com>
2019-11-01 12:56:15 +0100

    Remove onClientError handler

--- src/httpServer/index.ts
+++ src/httpServer/index.ts
@@ -1,7 +1,6 @@
 import { Server, createServer } from "http";
 
 import onRequest from "./onRequest";
-import onClientError from "./onClientError";
 import onError from "./onError";
 
 import ILogger from "../iLogger";
@@ -9,7 +8,6 @@ import IEndpoint from "../iEndpoint";
 
 export default (logger: ILogger, endpoints: IEndpoint[]): Server => {
   const server = createServer()
-    .on("clientError", onClientError(logger))
     .on("request", onRequest(logger, endpoints))
     .on("error", onError(logger));
 
